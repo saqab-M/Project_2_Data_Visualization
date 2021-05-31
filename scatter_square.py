e2a8ceb2d1dd237776dbcc20df20c6e3943b64b1
saqab-M <47401181+saqab-M@users.noreply.github.com>
2021-05-31 18:12:46 +0100

    Phase_1 scatter plot

--- scatter_square.py
+++ scatter_square.py
@@ -1,11 +1,11 @@
 import matplotlib.pyplot as plt
 
-x_values = [1, 2, 3, 4, 5]
-y_values = [1, 4, 9, 16, 25]
+x_values = range(1, 1_001)
+y_values = [x**2 for x in x_values]
 
 plt.style.use("seaborn")
 fig, ax = plt.subplots()
-ax.scatter(x_values, y_values, s=100)
+ax.scatter(x_values, y_values, c=y_values, cmap=plt.cm.Reds, s=10)
 
 #set chart title and label axes
 ax.set_title("Square Numbers", fontsize=24)
@@ -13,6 +13,9 @@ ax.set_xlabel("Value", fontsize=14)
 ax.set_ylabel("Sqare of value", fontsize=14)
 
 #set size of tick labels
-ax.tick_params(axis="both",which="major", labelsize=14)
+#ax.tick_params(axis="both",which="major", labelsize=14)
 
-plt.show()
+ax.axis([0, 1_100, 0, 1_100_000])
+
+#plt.show()
+plt.savefig("squares_plot.png", bbox_inches="tight")
